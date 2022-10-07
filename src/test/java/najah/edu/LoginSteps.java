102e7984e17c7811a73199f967f92736c45f5437
daliakhlaif <daliakhlaif@localhost>
2022-10-07 14:38:27 +0300

    friday - 2:39pm

--- src/test/java/najah/edu/LoginSteps.java
+++ src/test/java/najah/edu/LoginSteps.java
@@ -6,34 +6,38 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-// to define the steps in the login.feature file (Given, When, Then)
+
 public class LoginSteps {
 	
+	
+     MyApp app = new MyApp();
 
 	@Given("User opens application and goes to login page")
-	public void user_opens_application_and_goes_to_login_page() {
-	    System.out.println("user on Login page");
-	   
+	public void user_opens_application_and_goes_to_login_page(){
+		
+	   System.out.println("User in Login page");
+	
 	}
 
 	@Given("the following users exists:")
 	public void the_following_users_exists(DataTable users) {
 		
 		List<List<String>> userList = users.asLists(String.class);
-	    for(List<String> u : userList) {
-	    	System.out.println(u);
-	    }
+		
+		app.usersList = userList;
+		
+	   
 	}
 
 	@When("The user logs in with email {string} and password  {string}")
 	public void the_user_logs_in_with_email_and_password(String email, String password) {
-	    
+	    app.login(email, password);
 		
 	}
 
 	@Then("User should navigate to {string} page")
-	public void user_should_navigate_to_page(String homepage) {
-	    
+	public void user_should_navigate_to_page(String page) {
+	   
 	    
 	}
 
@@ -43,17 +47,6 @@ public class LoginSteps {
 	   
 	}
 
-	@Then("The user should get the message wrong email or paaword")
-	public void the_user_should_get_the_message_wrong_email_or_paaword() {
-		 System.out.println("wrong email or paaword");
-	    	}
-
-	@Then("The user should get the message wrong email or password")
-	public void the_user_should_get_the_message_wrong_email_or_password() {
-		 System.out.println("wrong email or paaword");
-	   
-	}
-
-
+	
 
 }
--- src/test/java/najah/edu/MyApp.java
+++ src/test/java/najah/edu/MyApp.java
@@ -1,5 +1,91 @@
 package najah.edu;
 
-public class MyApp {
+import java.util.List;
+import java.util.Scanner;
 
+public class MyApp {
+	
+	   List<List<String>> usersList;
+	   
+	void admin() {
+		Scanner sc=new Scanner(System.in);
+			
+			System.out.println("\t\t  _________________________");
+			System.out.println("\t\t |                         |");
+			System.out.println("\t\t |       Welcome Admin     |");
+			System.out.println("\t\t |_________________________|");
+	}
+	
+	
+	
+	void patient()
+	{
+	
+			System.out.println("\t\t  _________________________");
+			System.out.println("\t\t |                         |");
+			System.out.println("\t\t | Welcome to Patient Page!|");
+			System.out.println("\t\t |_________________________|");
+			System.out.println(" ");
+		
+		}
+	
+	
+	void doctor()
+	{
+		
+			System.out.println("\t\t  _________________________");
+			System.out.println("\t\t |                         |");
+			System.out.println("\t\t | Welcome to Doctor Page! |");
+			System.out.println("\t\t |_________________________|");
+			System.out.println(" ");
+		
+	}
+	
+	void login(String email, String password)
+	{
+		
+		System.out.println("\t\t  _________________________");
+		System.out.println("\t\t |                         |");
+		System.out.println("\t\t |         Login 		   |");
+		System.out.println("\t\t |_________________________|");
+
+		
+	   
+	    boolean found = false;
+	    int in =0;
+	    
+	    for(List<String> u : usersList) {
+	    	for(String e : u) {
+	    		if(e.matches(email)) {
+	    			found = true;
+	    			in = e.indexOf(password);
+	    		}
+	    		
+	    	}
+	    }
+	    
+	    String role= null;
+	    if(found) {
+	    	in +=2;
+	    	
+	    	for(List<String> u : usersList) {
+		    	for(String e : u) {
+		    	role =	e.valueOf(in);
+		    	}
+		    }
+	    	
+	    	if(role.equals("admin"))admin();
+	    	else if(role.equals("patient"))patient();
+	    	else if(role == "doctor")doctor();
+	    	
+	    }else {
+	    	System.out.println("wrong email or password");
+	    }
+ 		
+	}
+	
+	
+	
 }
+
+
