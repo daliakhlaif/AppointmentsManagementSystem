db3f9ceb468c7719389fc7f9b294dd729abe18a7
marahalrantisi <113754456+marahalrantisi@users.noreply.github.com>
2022-12-16 09:19:29 +0200

    Update BillingSteps.java

--- src/test/java/najah/edu/BillingSteps.java
+++ src/test/java/najah/edu/BillingSteps.java
@@ -1,7 +1,7 @@
 package najah.edu;
 
 import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.Equals;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
