4c4b42446a7d457322ac0077c6b8349a99ba5795
Dalia Khlaif <87690042+daliakhlaif@users.noreply.github.com>
2022-12-20 17:32:37 +0200

    Create TestSteps.java

---
+++ src/test/java/najah/edu/TestSteps.java
@@ -0,0 +1,24 @@
+package najah.edu;
+
+import static org.junit.Assert.assertEquals;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class TestSteps {
+	
+    Test t = new Test();
+    String str;
+	@When("we enter the year {string}")
+	public void we_enter_the_year(String string) {
+	   str = string;
+	}
+
+	@Then("then we should {int}")
+	public void then_we_should(Integer int1) {
+	 Integer n =  t.getYear(str);
+	 
+	 assertEquals(int1,n );
+	}
+
+}
